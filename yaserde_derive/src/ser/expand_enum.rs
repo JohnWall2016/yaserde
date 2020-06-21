a17ee0ec471e6b824c0d4377fb5d6643ff072d72
John Wall <5672618@qq.com>
2020-06-21 18:05:54 +0800

    update

--- yaserde_derive/src/ser/expand_enum.rs
+++ yaserde_derive/src/ser/expand_enum.rs
@@ -46,86 +46,135 @@ fn inner_enum_inspector(
           }
         }),
         Fields::Named(ref fields) => {
-          let enum_fields: TokenStream = fields
+          let map_nonattr_field = |field: YaSerdeField| {
+            let field_label = field.label();
+
+            if field.is_text_content() {
+              return Some(quote!(
+                let data_event = XmlEvent::characters(&self.#field_label);
+                writer.write(data_event).map_err(|e| e.to_string())?;
+              ));
+            }
+
+            let field_label_name = field.renamed_label(root_attributes);
+
+            match field.get_type() {
+              Field::FieldString
+              | Field::FieldBool
+              | Field::FieldU8
+              | Field::FieldI8
+              | Field::FieldU16
+              | Field::FieldI16
+              | Field::FieldU32
+              | Field::FieldI32
+              | Field::FieldF32
+              | Field::FieldU64
+              | Field::FieldI64
+              | Field::FieldF64 => Some({
+                quote! {
+                  match self {
+                    &#name::#label{ref #field_label, ..} => {
+                      let struct_start_event = XmlEvent::start_element(#field_label_name);
+                      writer.write(struct_start_event).map_err(|e| e.to_string())?;
+
+                      let string_value = #field_label.to_string();
+                      let data_event = XmlEvent::characters(&string_value);
+                      writer.write(data_event).map_err(|e| e.to_string())?;
+
+                      let struct_end_event = XmlEvent::end_element();
+                      writer.write(struct_end_event).map_err(|e| e.to_string())?;
+                    },
+                    _ => {},
+                  }
+                }
+              }),
+              Field::FieldStruct { .. } => Some(quote! {
+                match self {
+                  &#name::#label{ref #field_label, ..} => {
+                    writer.set_start_event_name(Some(#field_label_name.to_string()));
+                    writer.set_skip_start_end(false);
+                    #field_label.serialize(writer)?;
+                  },
+                  _ => {}
+                }
+              }),
+              Field::FieldVec { .. } => Some(quote! {
+                match self {
+                  &#name::#label{ref #field_label, ..} => {
+                    for item in #field_label {
+                      writer.set_start_event_name(Some(#field_label_name.to_string()));
+                      writer.set_skip_start_end(false);
+                      item.serialize(writer)?;
+                    }
+                  },
+                  _ => {}
+                }
+              }),
+              Field::FieldOption { .. } => None,
+            }
+          };
+
+          let map_attr_field = |field: YaSerdeField| {
+            let field_label = field.label();
+            let field_label_name = field.renamed_label(root_attributes);
+            match field.get_type() {
+              Field::FieldString => {
+                Some(quote! {
+                  match self {
+                    &#name::#label{ref #field_label, ..} => {
+                      struct_start_event = struct_start_event.attr(#field_label_name, &#field_label);
+                    },
+                    _ => {},
+                  }
+                })
+              }
+              _ => None,
+            }
+          };
+
+          let collect_tokens = |vec: Vec<Option<TokenStream>>| -> TokenStream {
+            vec.into_iter().filter_map(|x| x).collect()
+          };
+
+          let mut fields_tokens: Vec<Option<TokenStream>> = vec![];
+          let mut append_attrs: Vec<Option<TokenStream>> = vec![];
+
+          fields
             .named
             .iter()
             .map(|field| YaSerdeField::new(field.clone()))
-            .filter(|field| !field.is_attribute())
-            .map(|field| {
-              let field_label = field.label();
-
-              if field.is_text_content() {
-                return Some(quote!(
-                  let data_event = XmlEvent::characters(&self.#field_label);
-                  writer.write(data_event).map_err(|e| e.to_string())?;
-                ));
+            .for_each(|field| {
+              if field.is_attribute() {
+                append_attrs.push(map_attr_field(field))
+              } else {
+                fields_tokens.push(map_nonattr_field(field));
               }
+            });
 
-              let field_label_name = field.renamed_label(root_attributes);
+          let append_attrs = collect_tokens(append_attrs);
+          let fields_tokens = collect_tokens(fields_tokens);
 
-              match field.get_type() {
-                Field::FieldString
-                | Field::FieldBool
-                | Field::FieldU8
-                | Field::FieldI8
-                | Field::FieldU16
-                | Field::FieldI16
-                | Field::FieldU32
-                | Field::FieldI32
-                | Field::FieldF32
-                | Field::FieldU64
-                | Field::FieldI64
-                | Field::FieldF64 => Some({
-                  quote! {
-                    match self {
-                      &#name::#label{ref #field_label, ..} => {
-                        let struct_start_event = XmlEvent::start_element(#field_label_name);
-                        writer.write(struct_start_event).map_err(|e| e.to_string())?;
-
-                        let string_value = #field_label.to_string();
-                        let data_event = XmlEvent::characters(&string_value);
-                        writer.write(data_event).map_err(|e| e.to_string())?;
-
-                        let struct_end_event = XmlEvent::end_element();
-                        writer.write(struct_end_event).map_err(|e| e.to_string())?;
-                      },
-                      _ => {},
-                    }
-                  }
-                }),
-                Field::FieldStruct { .. } => Some(quote! {
-                  match self {
-                    &#name::#label{ref #field_label, ..} => {
-                      writer.set_start_event_name(Some(#field_label_name.to_string()));
-                      writer.set_skip_start_end(false);
-                      #field_label.serialize(writer)?;
-                    },
-                    _ => {}
-                  }
-                }),
-                Field::FieldVec { .. } => Some(quote! {
-                  match self {
-                    &#name::#label{ref #field_label, ..} => {
-                      for item in #field_label {
-                        writer.set_start_event_name(Some(#field_label_name.to_string()));
-                        writer.set_skip_start_end(false);
-                        item.serialize(writer)?;
-                      }
-                    },
-                    _ => {}
-                  }
-                }),
-                Field::FieldOption { .. } => None,
+          if append_attrs.is_empty() {
+            Some(quote! {
+              &#name::#label{..} => {
+                #fields_tokens
               }
             })
-            .filter_map(|x| x)
-            .collect();
+          } else {
+            Some(quote! {
+              &#name::#label{..} => {
+                let mut struct_start_event = XmlEvent::start_element(#label_name);
+                #append_attrs
 
-          Some(quote! {
-            &#name::#label{..} => {
-              #enum_fields
-            }
-          })
+                writer.write(struct_start_event).map_err(|e| e.to_string())?;
+
+                #fields_tokens
+
+                let struct_end_event = XmlEvent::end_element();
+                writer.write(struct_end_event).map_err(|e| e.to_string())?;
+              }
+            })
+          }
         }
         Fields::Unnamed(ref fields) => {
           let enum_fields: TokenStream = fields
